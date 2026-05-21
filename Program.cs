abefaddaa8f9cd7b458ef5af19bc970b61e16d75
camiglz <cami.glz30@gmail.com>
2026-05-21 14:10:48 +0000

    agregue elementos a mi stack

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Stack<string> pedidos = new Stack<string>();
+
+        pedidos.Push("Tacos");
+        pedidos.Push("Torta");
+        pedidos.Push("Agua");
+
+        Console.WriteLine(pedidos.Count);
+    }
+}
